867da52d78aecc17dd610d6e609603c33e5b2900
Viet Hung Nguyen <famihug@gmail.com>
2012-04-10 10:12:15 +0700

    done push change to db

--- admin/hula_about.php
+++ admin/hula_about.php
@@ -1,26 +1,41 @@
 
-<script type="text/javascript" src="../ext/fckeditor/fckeditor.js"></script>
-<script type="text/javascript" src="../ext/fckeditor/fckconfig.js"></script>
-<script language="JavaScript" type="text/javascript">
-	window.onload = function()
-	{
-		var fck = new FCKeditor('content') ; // ten cua text area
-		fck.BasePath    = '../ext/fckeditor/' ;
-		fck.Width = 746;
-		fck.Height = 300;
-		fck.ReplaceTextarea() ;
-	}
-</script>
-
-
-<textarea id="ckeditor" name="content" cols="150"> 
 <?php
-	$sql = "SELECT content FROM ".TABLE_HULA_OUR_INFOS." WHERE type='about'";
-	$result = tep_db_query($sql);
-
-	while(($row = tep_db_fetch_array($result)) != null)
-		echo $row['content'];
+include_once("../ext/fckeditor/fckeditor.php");
 ?>
-</textarea>
+<html>
+<head>
+  <title>FCKeditor - Sample</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<?php
+//push text to db
+if(isset($_POST['FCKeditor1']))
+{
+	$sValue = stripslashes( $_POST['FCKeditor1'] ) ;
+	$sql_update = "UPDATE ".TABLE_HULA_OUR_INFOS." SET content='".$sValue."' WHERE type='about'";
+	tep_db_query($sql_update);
+}
+?>
+<body>
 
+  <form action="" method="post">
+<?php
+//Get text from db
+$sql = "SELECT content FROM ".TABLE_HULA_OUR_INFOS." WHERE type='about'";
+$result = tep_db_query($sql);
 
+$textarea = '';
+while(($row = tep_db_fetch_array($result)) != null)
+	$textarea.= $row['content'];
+
+//Create fckeditor
+$oFCKeditor = new FCKeditor('FCKeditor1') ;
+$oFCKeditor->BasePath = '../ext/fckeditor/' ;
+$oFCKeditor->Value = $textarea ;
+$oFCKeditor->Create() ;
+?>
+	<br>
+	<input type="submit" value="Submit">
+  </form>
+</body>
+</html>
